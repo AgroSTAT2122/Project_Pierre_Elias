63cf7e528cf7335f088ef614ae3f63626b8c902e
Elias <eliashermance@gmail.com>
2021-11-25 17:42:39 +0100

    Ajout carte

--- Map.R
+++ Map.R
@@ -1,3 +1,6 @@
+library(viridis)
+
+
 # data processing
 train <- read_csv("data/Regularities_by_liaisons_Trains_France.csv")
 coord <- read_delim("data/Coordonnees.csv", delim = ";", escape_double = FALSE)
@@ -8,11 +11,12 @@ train <- train %>%
 
 # liaison map
 # edges: liaisons
+
 e_train <- train %>% select(`Departure station`, `Arrival station`,
-                            `Number of expected circulations`,`Number of cancelled trains`,`Number of trains late on arrival`) %>% 
+                            `Number of expected circulations`,`Number of cancelled trains`,`Number of trains late on arrival`,`Number of late trains at departure`) %>% 
   rename(Dep = `Departure station`, Arr = `Arrival station`) %>% 
   group_by(Dep, Arr) %>% 
-  summarise(Nb = sum(`Number of expected circulations`-`Number of cancelled trains`, na.rm = TRUE), Nb_Delay = sum(`Number of trains late on arrival`,na.rm=TRUE))
+  summarise(Nb = sum(`Number of expected circulations`-`Number of cancelled trains`, na.rm = TRUE), Nb_Delay = sum(`Number of trains late on arrival`,na.rm=TRUE), Nb_Delay_dep = sum(`Number of late trains at departure`), na.rm = TRUE)
 
 e_train <- e_train %>% 
   filter(str_detect(Dep,"PARIS")|str_detect(Arr,"PARIS"))
@@ -23,7 +27,7 @@ e_trajet <- read_delim("data/e_trajet.csv", delim = ";",
 e_train <- as.data.frame(c(e_train, e_trajet[,4:5]))
 
 e_test <- e_train %>% 
-  group_by(Trajet.A,Trajet.B) %>% summarise(Tot = sum(Nb), Delay = sum(Nb_Delay))
+  group_by(Trajet.A,Trajet.B) %>% summarise(Tot = sum(Nb), Delay = sum(Nb_Delay), Delay_dep = sum(Nb_Delay_dep))
 
 for (i in 1:nrow(e_test)){
   for (j in 1:nrow(y_train)){
@@ -34,20 +38,17 @@ for (i in 1:nrow(e_test)){
 }
 
 Tot <- e_test %>% group_by(Direction) %>% 
-  summarise(Total = sum(Tot), Delay = sum(Delay))
+  summarise(Total = sum(Tot), Delay = sum(Delay),Delay_dep = sum(Delay_dep))
 
-Netdf <- cbind(rep("PARIS",4),Cardinaux$Direction)
-Netdf <- as.data.frame(Netdf)
-
-colnames(Netdf) <- c("PARIS", "DIRECTION")
 
 Fleches <- read_delim("data/Fleches.csv", delim = ";", 
                       escape_double = FALSE, trim_ws = TRUE)
 
 Cardinaux <- cbind(Tot,Fleches)
 
-Cardinaux$Percent <- (Cardinaux$Delay/Cardinaux$Total)
-Paris <- c("PARIS",sum(Cardinaux$Total),sum(Cardinaux$Delay),"PARIS",	2.3522219, 	48.856614, (sum(Cardinaux$Delay)/sum(Cardinaux$Total)))
+Cardinaux$Percent <- (Cardinaux$Delay/Cardinaux$Total)*100
+Cardinaux$Percent_dep <- (Cardinaux$Delay_dep/Cardinaux$Total)*100
+Paris <- c("PARIS",sum(Cardinaux$Total),sum(Cardinaux$Delay),sum(Cardinaux$Delay_dep),"PARIS",	2.3522219, 	48.856614, (sum(Cardinaux$Delay)/sum(Cardinaux$Total))*100, (sum(Cardinaux$Delay_dep)/sum(Cardinaux$Total))*100)
 Paris <- t(as.data.frame(Paris))
 colnames(Paris) <- colnames(Cardinaux)
 
@@ -55,79 +56,57 @@ Jdd <- rbind(Cardinaux, Paris)
 rownames(Jdd) <- Jdd$Direction 
 Jdd$Total <- as.numeric(Jdd$Total)
 Jdd$Delay <- as.numeric(Jdd$Delay)
+Jdd$Delay_dep <- as.numeric(Jdd$Delay_dep)
 Jdd$Longitude <- as.numeric(Jdd$Longitude)
 Jdd$Latitude <- as.numeric(Jdd$Latitude)
 Jdd$Percent <- as.numeric(Jdd$Percent)
-
-#############
-
-NORD_EST <- network(Net[1,],directed = TRUE,loops = TRUE)
-SUD_EST <- network(Net[3,])
-NORD_OUEST <- network(Net[2,],directed = TRUE,loops = TRUE)
-SUD_OUEST <- network(Net[4,],directed = TRUE,loops = TRUE)
-
-
-SUD_EST %v% "lat" <- Jdd[network.vertex.names(SUD_EST),"Latitude"]
-SUD_EST %v% "lon" <- Jdd[network.vertex.names(SUD_EST),"Longitude"]
-SUD_EST %v% "Delay" <- Jdd[network.vertex.names(SUD_EST),"Percent"]
-# add random groups
-SUD_EST %v% "mygroup" <- Jdd[network.vertex.names(SUD_EST),"Direction"]
-SUD_EST %v% "degree" <- degree(SUD_EST, gmode = "graph")
-
-
-NORD_OUEST %v% "lat" <- Jdd[network.vertex.names(NORD_OUEST),"Latitude"]
-NORD_OUEST %v% "lon" <- Jdd[network.vertex.names(NORD_OUEST),"Longitude"]
-NORD_OUEST %v% "Delay" <- Jdd[network.vertex.names(NORD_OUEST),"Percent"]
-NORD_OUEST %v% "mygroup" <- Jdd[network.vertex.names(sens),"Direction"]
-
-SUD_OUEST %v% "lat" <- Jdd[network.vertex.names(SUD_OUEST),"Latitude"]
-SUD_OUEST %v% "lon" <- Jdd[network.vertex.names(SUD_OUEST),"Longitude"]
-SUD_OUEST %v% "Delay" <- Jdd[network.vertex.names(SUD_OUEST),"Percent"]
-SUD_OUEST %v% "mygroup" <- Jdd[network.vertex.names(sens),"Direction"]
-
-NORD_EST %v% "lat" <- Jdd[network.vertex.names(NORD_EST),"Latitude"]
-NORD_EST %v% "lon" <- Jdd[network.vertex.names(NORD_EST),"Longitude"]
-NORD_EST %v% "Delay" <- Jdd[network.vertex.names(NORD_EST),"Percent"]
-NORD_EST %v% "mygroup" <- Jdd[network.vertex.names(sens),"Direction"]
+Jdd$Percent_dep <- as.numeric(Jdd$Percent_dep)
 
 # create a map of the Europe
-europa <- ggplot(test, aes(x = long, y = lat)) +
-  geom_polygon(aes(group = group), color = "grey65",
+europa <- ggplot() +
+  geom_polygon(aes(x = test$long, y = test$lat, group = test$group), color = "grey65",
                fill = "#f9f9f9", size = 0.2) +  
-  coord_map(xlim = c(-5, 10.5),ylim = c(40, 51))
-
-europa %>% ggnetworkmap(net = SUD_EST, size = c(1,10), great.circles = TRUE,
-                        segment.color = "steelblue", arrow.size = 0,
-                        node.group = mygroup, alpha = 0.5, segment.size = 1) %>% 
-  ggnetworkmap(net = NORD_OUEST, size = 10, great.circles = TRUE,
-               segment.color = "red", arrow.size = 0,
-               node.group = mygroup, alpha = 0.5, segment.size = 1) %>% 
-  ggnetworkmap(net = NORD_EST, size = 10, great.circles = TRUE,
-               segment.color = "red", arrow.size = 0,
-               node.group = mygroup, alpha = 0.5, segment.size = 1) %>% 
-  ggnetworkmap(net = SUD_OUEST, size = 10, great.circles = TRUE,
-               segment.color = "yellow", arrow.size = 0,
-               node.group = mygroup, alpha = 0.5, segment.size = 1)
-
-#######
-
-Net <- network(Net,directed = TRUE,loops = TRUE)
-
-
-Net %v% "lat" <- Jdd[network.vertex.names(Net),"Latitude"]
-Net %v% "lon" <- Jdd[network.vertex.names(Net),"Longitude"]
-Net %v% "Delay" <- Jdd[network.vertex.names(Net),"Percent"]
-Net %v% "mygroup" <- Jdd[network.vertex.names(Net),"Direction"]
-Net %v% "degree" <- degree(Net, gmode = "graph")
-
-# create a map of the Europe
-europa <- ggplot(test, aes(x = long, y = lat)) +
-  geom_polygon(aes(group = group), color = "grey65",
-               fill = "#f9f9f9", size = 0.2) +  
-  coord_map(xlim = c(-5, 10.5),ylim = c(40, 51))
+  coord_map(xlim = c(-5, 8),ylim = c(42, 51))
 
 europa %>% ggnetworkmap(net = Net, size = 5, great.circles = TRUE,
-                        segment.color = "blue", node.group = mygroup,
-                        arrow.size = 0,
+                        node.group = mygroup,
+                        arrow.size = 0, 
                         alpha = 0.5, segment.size = 2,label.nodes = TRUE)
+
+arrow()
+
+point <- geom_point(aes(x = Jdd$Longitude, y = Jdd$Latitude, color = Jdd$Direction), inherit.aes = FALSE, size = 8)
+color_point <- c("#440154FF","#31688EFF","grey","#35B779FF","#FDE725FF")
+
+segment <- geom_segment(aes(x = Jdd$Longitude[5], xend = Jdd$Longitude[-5], y = Jdd$Latitude[5], yend = Jdd$Latitude[-5]), size = 2,inherit.aes = FALSE, arrow = arrow()) 
+
+
+europa +
+  geom_segment(aes(x = Jdd$Longitude[5], xend = Jdd$Longitude[-5], y = Jdd$Latitude[5], yend = Jdd$Latitude[-5], color = Jdd$Percent[-5]), size = 2,inherit.aes = FALSE, arrow = arrow()) +
+  labs(color = "Trains en retard à l'arrivée (en %)") +
+  scale_color_gradient("Trains en retard à l'arrivée (en %)", high = "red", low = "yellow") 
+
+
+
+europa + geom_point(aes(x = Jdd$Longitude, y = Jdd$Latitude, color = Jdd$Direction), inherit.aes = FALSE, size = c(8,8,8,8,5)) +
+  scale_color_manual(values = color_point, guide = "none") +
+  geom_segment(aes(x = Jdd$Longitude[5], xend = Jdd$Longitude[-5], y = Jdd$Latitude[5], yend = Jdd$Latitude[-5], color = Jdd$Percent), size = 2,inherit.aes = FALSE, arrow = arrow()) +
+  theme(panel.background = element_blank(), #On enlève le fond
+        axis.title = element_blank(), #On enlève le titre des axes
+        axis.ticks = element_blank(), #On enlève les graduations
+        axis.text = element_blank()) #On enlève le texte des axes
+  
+
+europa + geom_point(aes(x = Jdd$Longitude, y = Jdd$Latitude, fill = Jdd$Direction, size = Jdd$Total), inherit.aes = FALSE, shape = 21) + scale_size(range = c(7,14)) +
+  scale_fill_manual(values = color_point, guide = "none") + labs(size = "Nombre Total de Trajets effectués") +
+  geom_segment(aes(x = Jdd$Longitude[5], xend = Jdd$Longitude[-5], y = Jdd$Latitude[5], yend = Jdd$Latitude[-5], color = Jdd$Percent[-5]), size = 2,inherit.aes = FALSE, arrow = arrow()) +
+  labs(color = "Trains en retard à l'arrivée (en %)") +
+  scale_color_gradient("Trains en retard à l'arrivée (en %)", high = "darkblue", low = "cyan")  +
+  theme(panel.background = element_blank(), #On enlève le fond
+        axis.title = element_blank(), #On enlève le titre des axes
+        axis.ticks = element_blank(), #On enlève les graduations
+        axis.text = element_blank()) + #On enlève le texte des axes 
+  geom_text(aes(x = Jdd$Longitude, y = Jdd$Latitude) ,label = rownames(Jdd), nudge_x = 0, nudge_y = c(0.65,0.65,-0.55,-0.45,0.65))
+
+
 
